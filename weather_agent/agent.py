5bf4df8865b3ae93db1cbd42bbefdaced02f8a12
brajwnl <braj.s@wellnessliving.com>
2026-06-07 20:47:06 +0530

    add structured output

--- weather_agent/agent.py
+++ weather_agent/agent.py
@@ -3,6 +3,8 @@
 from dotenv import load_dotenv
 from openai import OpenAI
 import requests
+from pydantic import BaseModel, Field
+from typing import Optional
 
 import json
 
@@ -75,38 +77,43 @@ SYSTEM_PROMPT = """
 
  """
 
-
 print("\n\n\n")
 
 message_history = [
 
         {"role":"system", "content": SYSTEM_PROMPT},
 
-
 ]
+
+
+class OutputFormat(BaseModel):
+    step:str = Field(..., description="The ID of the step. Example: PLAN, OUTPUT, TOOL")
+    content:Optional[str] = Field(None, description="The optional string content for the step")
+    tool:Optional[str] = Field(None, description="The ID of the tool to call")
+    input:Optional[str] = Field(None, description="The input param of the tool")
 
 while True:
     user_query = input("👈")
     message_history.append({"role":"user", "content":user_query})
 
     while True:
-        response = client.chat.completions.create(
+        response = client.chat.completions.parse(
                 model="gpt-4o-mini",
-                response_format={"type":"json_object"},
+                response_format= OutputFormat,
                 messages=message_history
         )
 
-        raw_result = (response.choices[0].message.content)
+        raw_result = response.choices[0].message.content
         message_history.append({"role":"assistant", "content": raw_result})
-        parsed_result = json.loads(raw_result)
+        parsed_result = response.choices[0].message.parsed
 
-        if parsed_result.get("step") == "START":
-            print("🔥", parsed_result.get("content"))
+        if parsed_result.step == "START":
+            print("🔥", parsed_result.content)
             continue
 
-        if parsed_result.get("step") == "TOOL":
-            tool_to_call = parsed_result.get("tool")
-            tool_input = parsed_result.get("input")
+        if parsed_result.step == "TOOL":
+            tool_to_call = parsed_result.tool
+            tool_input = parsed_result.input
             print(f"🔎:{tool_to_call} ({tool_input})")
             tool_response = available_tools[tool_to_call](tool_input)
             print(f"🔎:{tool_to_call} ({tool_input})= {tool_response}")
@@ -114,12 +121,12 @@ while True:
                 "step": "OBSERVE", "tool": tool_to_call, "input": tool_input, "output": tool_response
             })})
 
-        if parsed_result.get("step") == "PLAN":
-            print("🧠", parsed_result.get("content"))
+        if parsed_result.step == "PLAN":
+            print("🧠", parsed_result.content)
             continue
 
-        if parsed_result.get("step") == "OUTPUT":
-            print("🤖", parsed_result.get("content"))
+        if parsed_result.step == "OUTPUT":
+            print("🤖", parsed_result.content)
             break
 
 
