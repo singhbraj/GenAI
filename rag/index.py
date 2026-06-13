107c9f64f0a0a58cc03dcf5c335a080d55f5a97f
brajwnl <braj.s@wellnessliving.com>
2026-06-14 02:36:00 +0530

    indexing pipeline is done

--- rag/index.py
+++ rag/index.py
@@ -1,10 +1,15 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
 
 pdf_path = Path(__file__).parent / "nodejs.pdf"
 
 loader = PyPDFLoader(str(pdf_path))
+
+load_dotenv()
 
 docs = loader.load()
 
@@ -14,4 +19,19 @@ print(docs[2])
 
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=400)
-chunks = text_splitter.split_text(documents = docs)
+chunks = text_splitter.split_documents(documents=docs)
+
+# Vextor Embeddings 
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-large"
+)
+
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding=embeddings,
+    url="http://localhost:6333",
+    collection_name="rag"
+)
+
+print("Indexing of documents done.")
